06687f5596525bd7bf26ed3a6bde3d91ee9562a6
Samuel Barragan <samuel.barragan34@hotmail.com>
2026-02-04 20:01:34 -0800

    new version

--- src/orders/index.ts
+++ src/orders/index.ts
@@ -1,4 +1,7 @@
+// Orders
 export { default as Order } from "./models/Orders"
+export { OrderStatus, PaymentMethod, PaymentStatus } from "./models/Orders"
+
+// Order status logs
 export { default as OrderStatusLog } from "./models/OrderStatusLogs"
-export { OrderStatus } from "./models/Orders"
 export type { OrderStatusLogType } from "./models/OrderStatusLogs"
