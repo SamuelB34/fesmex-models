d12fd9e882d19ab100bc2b92cc916eafa710863d
Samuel Barragan <samuel.barragan34@hotmail.com>
2026-02-21 18:40:07 -0800

    Add files to articles

--- src/inventory/models/Article.ts
+++ src/inventory/models/Article.ts
@@ -1,5 +1,18 @@
 import mongoose, { Schema, model, Types } from "mongoose"
 
+const articleFileSchema = new Schema(
+	{
+		key: { type: String, required: true },
+		url: { type: String, required: true },
+		filename: { type: String, required: true },
+		mime_type: { type: String, required: true },
+		size: { type: Number, required: true },
+		uploaded_at: { type: Date, default: Date.now },
+		uploaded_by: { type: String, required: true },
+	},
+	{ _id: false }
+)
+
 const articleSchema = new Schema({
 	article_number: { type: String, required: true, unique: true }, // SAP ItemCode
 	description: { type: String, required: true },
@@ -7,6 +20,10 @@ const articleSchema = new Schema({
 	brand: { type: String },
 	model: { type: String },
 	group_id: { type: Types.ObjectId, ref: "ArticleGroup", required: true },
+	files: {
+		images: { type: [articleFileSchema], required: false },
+		datasheets: { type: [articleFileSchema], required: false },
+	},
 	created_at: { type: Date, default: Date.now },
 	updated_at: { type: Date },
 	deleted_at: { type: Date },
